fbf6c2504cf6f5d70791211dcedda9d83335ae07
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2024-04-26 02:20:00 +0700

    Update complex.cpp

--- 2/complex.cpp
+++ 2/complex.cpp
@@ -3,39 +3,44 @@
 
 using namespace std;
 
-complex<double> polynomial(complex<double> z) {
+complex<double> func(complex<double> z) {
     return pow(z, 3.0) - 1.0;
 }
 
-complex<double> derivative(complex<double> z) {
+complex<double> der(complex<double> z) {
     return 3.0 * pow(z, 2.0);
 }
 
-complex<double> newtonMethod(complex<double> initialGuess, double epsilon, int maxIterations) {
-    complex<double> z = initialGuess;
-    int iterations = 0;
+complex<double> newton(complex<double> init, double eps, int iters) {
+    complex<double> z = init;
+    int curr_iters = 0;
 
-    while (abs(polynomial(z)) > epsilon && iterations < maxIterations) {
-        z = z - polynomial(z) / derivative(z);
-        iterations++;
+    while (abs(func(z)) > eps && curr_iters < iters) {
+        z = z - func(z) / der(z);
+        curr_iters++;
     }
 
     return z;
 }
 
 int main() {
-    double epsilon = 1e-6; 
-    int maxIterations = 1000;
+    double eps = 1e-6;
+    int iters = 1000;
 
-    complex<double> initialGuesses[3] = {complex<double>(1.0, 0.0), 
-                                          complex<double>(-0.5, 0.5 * sqrt(3.0)), 
-                                          complex<double>(-0.5, -0.5 * sqrt(3.0))};
+    complex<double> init[3] = { complex<double>(2.3, 0.0),
+                                complex<double>(-0.424, 0.2235 * sqrt(3.0)),
+                                complex<double>(-0.3523, -0.152 * sqrt(3.0)) };
 
     cout << "Solutions z^3 - 1:\n";
     for (int i = 0; i < 3; ++i) {
-        complex<double> root = newtonMethod(initialGuesses[i], epsilon, maxIterations);
-        cout << "Solution: " << i + 1 << ": " << root << endl;
+        complex<double> root = newton(init[i], eps, iters);
+        cout << "Solution: " << i + 1 << ": " << root.real() << " + i*" << root.imag() << endl;
     }
-
+    /* 
+        ROOTS:
+            1 + 0i
+            -1/2 - i*sqrt(3)/2 
+            -1/2 + i*sqrt(3)/2
+    */
     return 0;
 }
