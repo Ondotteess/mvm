e2af07a0ea5dec5425268fea5b8af50a34e256e6
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2024-04-26 00:32:30 +0700

    Update bisect.cpp

--- 2/bisect.cpp
+++ 2/bisect.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 
 double func(double x) {
     return tan(x) - x;
@@ -13,10 +14,13 @@ double bisection(double a, double b, double eps) {
 
     double c = a;
     while ((b - a) >= eps) {
+
+        //std::cout << "[ " << a << ", " << b <<  "]\n\n";
+
         c = (a + b) / 2;
 
         if (func(c) == 0.0)
-            break;
+            break;              
         else if (func(c) * func(a) < 0)
             b = c;
         else
@@ -26,12 +30,22 @@ double bisection(double a, double b, double eps) {
 }
 
 int main() {
-    double a = 1, b = 2; // segment
-    double eps = 0.01;   // accuracy
 
-    std::cout << "Solution: " << bisection(4.4, 4.6, 0.1) << " Accuracy: 0.1" << std::endl;
-    std::cout << "Solution: " << bisection(3, 4, 0.01) << " Accuracy: 0.01" << std::endl;
-    std::cout << "Solution: " << bisection(-1.25, 0.625, 0.001) << " Accuracy: 0.001" << std::endl;
+    //segments
+    std::vector<double> first = {-1.5, 1} ; 
+    std::vector<double> second = { 4.4, 4.6 };
+    std::vector<double> third = { 10.6, 10.99 };
+
+    double eps = 0.0001;   
+
+    std::cout << "Solution: " << bisection(first[0], first[1], eps) 
+        << " Accuracy: " << eps << std::endl << std::endl;
+
+    std::cout << "Solution: " << bisection(second[0], second[1], eps) 
+        << " Accuracy: " << eps << std::endl << std::endl;
+
+    std::cout << "Solution: " << bisection(third[0], third[1], eps) 
+        << " Accuracy: " << eps << std::endl << std::endl;
 
 
     return 0;
