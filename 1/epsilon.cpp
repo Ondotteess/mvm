f638e15a0c2ce7e6e211ee7ceb10908ba78f5968
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2024-04-27 04:50:32 +0700

    Update epsilon.cpp

--- 1/epsilon.cpp
+++ 1/epsilon.cpp
@@ -18,21 +18,52 @@ int main() {
         counter_double += 1;
     }
 
-    std::cout << "Pow for float: "  << counter_float  << std::endl;
-    std::cout << "Pow for double: " << counter_double << std::endl;
+    std::cout << epsilon_float << std::endl;
+    std::cout << epsilon_double << std::endl;
+
+    //bias = 2^(w-1) - 1 
+
+    std::cout << "W for float: " << 32 - counter_float - 1 << std::endl;
+    //  Emax  =  2^2^7 - 1  =  2^(127) - 1
+    //  Emin  = -2^(127) + 2
+
+    float a = 1;
+    for (int i = 0; i < 128; i++) {
+        a *= 2;
+    }
+    std::cout << a << std::endl;        // inf
+
+
+    std::cout << "W for double: " << 64 - counter_double << std::endl;
+    //  Emax  =>  2^(1023) - 1
+    //  Emin  =>  -2^(1023) + 2
+
+    double b = 1;
+    for (int i = 0; i < 1024; i++) {
+        b *= 2;
+    }
+    std::cout << b << std::endl;        // inf
 
     // |_ _..._ _..._|
-    //  S   E     M
+    //  S   E     M         float
     //  1   8     23
 
+    // |_ _..._ _..._|
+    //  S   E     M         double
+    //  1   11     52
+
+
     std::cout << (1 < 1 + epsilon_float) << std::endl;
+    std::cout << (1 == 1 + epsilon_float / 2) << std::endl;   
+    std::cout << (1 + epsilon_float > 1 + epsilon_float / 2) << std::endl;  // По определению eps
 
-    std::cout << (1 == 1 + epsilon_float / 2) << std::endl;
+    std::cout << (1 + epsilon_float + epsilon_float / 2 >
+        1 + epsilon_float / 2 + epsilon_float) << std::endl;    // Из-за неассоциативности
 
-    std::cout << (1 + epsilon_float == 1 + epsilon_float / 2) << std::endl;
 
-    std::cout << (1 + epsilon_float + epsilon_float / 2 == \
-                  1 + epsilon_float / 2 + epsilon_float ) << std::endl;
+
+    std::cout << (1 + epsilon_float < 1 + epsilon_float + epsilon_float / 2)
+        << std::endl;
 
 
     return 0;
